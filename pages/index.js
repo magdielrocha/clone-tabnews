0737ef24d424da5473ed086e17f9f74625b443cb
Magdiel Rocha <33636082+magdielrocha@users.noreply.github.com>
2025-01-08 20:09:31 +0000

    altera a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi amor, eu amo você! Se você me ama, dê uma risadinha 😎</h1>
+    return <h1>Oi Lucasbel! Se liga, esse site está no ar 🫡</h1>
 }
 
 export default Home;
